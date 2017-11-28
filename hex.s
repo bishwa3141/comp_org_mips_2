1489f5dda5c0e91984ab6880615ed16c4988be05
3141bishwa <bishwa.slwl@gmail.com>
2017-11-27 23:50:57 -0500

    Added checking for spaces before and after substring and working on the print subprogram

--- hex.s
+++ hex.s
@@ -34,6 +34,7 @@
 		addu 	$a1, $a1, 	1							#Incrementing the end index until ',' is found
 		add 	$t0, $t0, 	1							#Incrementing the address
 
+
 		beqz 	$s0, 			end_of_loop			#If "\0" found, end of loop
 
 		beq  	$s0, 	10, 	end_of_loop			#For string less than 9, the last characters
@@ -44,49 +45,65 @@
 
 		# When a ',' is found, call subprogram_2 to find the decimal value of the string, pass the start and end index of the string
 
-		jal subprogram_2:						# Passing $a0 and $a1 as argument to the function.
+		la $s1, 0 					# Keeing track of the length of the string being considered
+		la $s2, 0					# Result
+		la $s3, 0					# Boolean for checking if a space is found after a valid character
+		la $s4, 0					# Boolean for valid character found
 
-		jal subprogram_3:   					# Use the output from subprogram_2 to print the decimal value of the hex string
+
+		jal subprogram_1:						# Passing $a0 and $a1 as argument to the function.
+
+		jal subprogram_2:   					# Use the output from subprogram_2 to print the decimal value of the hex string
 
 		move $a0, $a1							# Set the new starting index to the end of the old starting index
 		
 		addu $a0, 2								# Ignoring the ',' for both of the indices
 		addu $a1, 2
 
+	subprogram_1:
+
+		beqz $a0, $a1  subprogram_3			# If the starting and ending index match, we have reached the end of the string. so
+											# the result of the string.
+		
+		lb  $s5, $a0($t0)					# Starting with the first index of the substring
+		
+		add 	$a0, $a0, 	1
+
+		beq 	$a0, 	32, 	space				# Case for when a space character is found
+		
+		j subprogram_3		
+
+
 	subprogram_2:
 
-		la $s4, 0 					#Length of the string being considered
-
-		bgt $s4, 8, too_long
-
-		la $s1, 0					# Result
-		la $s3, 0					# Boolean for checking if a space is found after a valid character
-		la $s4, 0					# Boolean for valid character found
 
 
+		blt $s5, 48, invalid 		# checks if the number is less than 48. Goes to invalid if true
 
-		addu $s4, $s4, 1			#incrementing the counter for the length of string
-
-		blt $a0, 48, invalid 		# checks if the number is less than 48. Goes to invalid if true
-
-		ble $a0, 57, valid_num		# Checks if the ASCII of character is less than 58. At this point,
+		ble $s5, 57, valid_num		# Checks if the ASCII of character is less than 58. At this point,
 									# already greater than 48, so it is a valid num in the range 0-9
 
-		blt $a0, 65, invalid 		# Checks if the ASCII of character is less than 68. At this point,
+		blt $s5, 65, invalid 		# Checks if the ASCII of character is less than 68. At this point,
 									# already greater than 58, so if true it is an invalid character
 
-		ble $a0, 70, valid_capital	# Checks if the ASCII of character is less than 68. At this point,
+		ble $s5, 70, valid_capital	# Checks if the ASCII of character is less than 68. At this point,
 									# already greater than 61, so if true it is a valid char in the range A - F
 
-		blt $a0, 96, invalid 		# Checks if the ASCII of character is less than 96. At this point,
+		blt $s5, 96, invalid 		# Checks if the ASCII of character is less than 96. At this point,
 									# already greater than 70, so if true it is an invalid character
 
-		ble $a0, 102, valid_small	# Checks if the ASCII of character is less than 58. At this point,
+		ble $s5, 102, valid_small	# Checks if the ASCII of character is less than 58. At this point,
 									# already greater than 96, so if true, it is a valid num in the range a-f
 
 		j invalid 					#At this point, already greater than 103, so it is invalid
 
 
-
 	subprogram_3:
+		
 
+	space:
+		beqz	$s4, subprogram_2			# Checks if a character has already been found
+
+		la 	$s3, 1							# Making note of a space found after a character is found
+
+		j loop_through						# return back to the loop
