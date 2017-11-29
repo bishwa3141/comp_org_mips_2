dff0f2c9242975ee4468173995df4d267bac6d89
3141bishwa <bishwa.slwl@gmail.com>
2017-11-28 23:40:05 -0500

    Renamed register names based on the guidelines given in the course slides

--- hex.s
+++ hex.s
@@ -50,47 +50,54 @@
 		la $s3, 0					# Boolean for checking if a space is found after a valid character
 		la $s4, 0					# Boolean for valid character found
 
-		jal subprogram_1:						# Passing $a0 and $a1 as argument to the function.
+		jal subprogram_2:						# Passing $a0 and $a1 as argument to the function.
 
-		jal subprogram_2:   					# Use the output from subprogram_2 to print the decimal value of the hex string
+		jal subprogram_3:   					# Use the output from subprogram_2 to print the decimal value of the hex string
 
 		move $a0, $a1							# Set the new starting index to the end of the old starting index
 		
 		addu $a0, 2								# Ignoring the ',' for both of the indices
 		addu $a1, 2
 
-	subprogram_1:
+	subprogram_2:
 
 		beqz $a0, $a1  subprogram_3			# If the starting and ending index match, we have reached the end of the string. so
 											# the result of the string. The variables are passed to the subprogram using stack
 		
 		lb  $s5, $a0($t0)					# Starting with the first index of the substring
 		
+
+		beq 	$s5, 	32, 	space		# Case for when a space character is found
+		
+		move $a0, $s5
+
+		jal subprogram_1		
+
+
+		sll 	$s2,	$s2, 	4 			# Multiplying the result by 16 using bit-shift	
+		addu 	$s2,	$s2, 	$v0 		# Adding the value of the character to the result
+
 		add 	$a0, $a0, 	1
 
-		beq 	$a0, 	32, 	space				# Case for when a space character is found
-		
-		jal subprogram_2		
-
-		j 	subprogram_1
+		j 	subprogram_2
 
 
-	subprogram_2:
-		blt $s5, 48, invalid 		# checks if the number is less than 48. Goes to invalid if true
+	subprogram_1:
+		blt $a0, 48, invalid 		# checks if the number is less than 48. Goes to invalid if true
 
-		ble $s5, 57, valid_num		# Checks if the ASCII of character is less than 58. At this point,
+		ble $a0, 57, valid_num		# Checks if the ASCII of character is less than 58. At this point,
 									# already greater than 48, so it is a valid num in the range 0-9
 
-		blt $s5, 65, invalid 		# Checks if the ASCII of character is less than 68. At this point,
+		blt $a0, 65, invalid 		# Checks if the ASCII of character is less than 68. At this point,
 									# already greater than 58, so if true it is an invalid character
 
-		ble $s5, 70, valid_capital	# Checks if the ASCII of character is less than 68. At this point,
+		ble $a0, 70, valid_capital	# Checks if the ASCII of character is less than 68. At this point,
 									# already greater than 61, so if true it is a valid char in the range A - F
 
-		blt $s5, 96, invalid 		# Checks if the ASCII of character is less than 96. At this point,
+		blt $a0, 96, invalid 		# Checks if the ASCII of character is less than 96. At this point,
 									# already greater than 70, so if true it is an invalid character
 
-		ble $s5, 102, valid_small	# Checks if the ASCII of character is less than 58. At this point,
+		ble $a0, 102, valid_small	# Checks if the ASCII of character is less than 58. At this point,
 									# already greater than 96, so if true, it is a valid num in the range a-f
 
 		j invalid 					#At this point, already greater than 103, so it is invalid
@@ -107,24 +114,18 @@
 
 	valid_num:
 
-		sll 	$s2,	$s2, 	4 				# Multiplying the result by 16 using bit-shift	
 		subu 	$v0, 	$a0, 	48				# Finding the real value of the character by subtracting
-		
-		j loop_through
+		jr $rs									#  Takes you back to the end of the instruction 'jal subprogram_2'
 
 	valid_capital:
 
-
-		sll 	$s1,	$s1, 	4				# Multiplying the result by 16 using bit-shift
 		subu 	$v0, 	$a0, 	55				# Finding the real value of the character by subtracting
-		
-		j loop_through
+		jr $rs 									#  Takes you back to the end of the instruction 'jal subprogram_2'
 
 
 	valid_small:
 		subu 	$v0, 	$a0, 	87				# Finding the real value of the character by subtracting
-
-		j loop_through
+		jr $rs 									#  Takes you back to the end of the instruction 'jal subprogram_2'
 
 
 	space:
